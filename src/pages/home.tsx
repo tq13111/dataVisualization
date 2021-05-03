dae6f163c97b28147f1c6d675c04289e36f81a1b
TQ <756342530@qq.com>
2021-05-03 15:04:34 +0800

    添加 header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './home.scss';
-
+import headerBg from '../images/header.png';
 export const Home = () => {
   return (
-    <div>
-      <div className="x">11</div>
+    <div className="home">
+      <header style={{backgroundImage:`url(${headerBg})`}}/>
     </div>
   );
-};
+}
